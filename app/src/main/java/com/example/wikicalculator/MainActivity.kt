fa169501b56fe1ec9cc238f18b6e873908fbccf8
lemondoglol <lemondoglol@github.com>
2023-04-01 10:47:59 -0400

    app init

---
+++ app/src/main/java/com/example/wikicalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wikicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
